9eda5c8f745c3fdd0a3ecaf3495b21e33a5c9463
Skiym <abramjosh13@gmail.com>
2025-02-02 13:22:26 +0800

    fix: add authorization and controller services

--- api/Program.cs
+++ api/Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 
@@ -53,6 +55,7 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -67,6 +70,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
-app.UseAuthorization();
+app.UseAuthorization(); 
+
+app.MapControllers();
 
 app.Run();
