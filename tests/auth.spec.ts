7c7661c8e15f2cdfa83cfe4691eda09efd4d3aaf
up-jacky <149249055+up-jacky@users.noreply.github.com>
2025-02-13 01:45:20 +0800

    fix(test): fix auth test cases

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,11 +1,10 @@
 const { test, describe, beforeEach, expect, afterAll, before } = require('@playwright/test')
 
 const loginWith = async (page, username, password) => {
-    await page.getByTestId('username').fill(username)
-    await page.getByTestId('password').fill(password)
-    await page.getByRole('button', { name: 'Login' }).click()
+    await page.getByTestId('username').fill(username);
+    await page.getByTestId('password').fill(password);
+    await page.getByRole('button', { name: 'Login' }).click();
 }
-
 
 describe('Authentication measures', () => {
 
@@ -33,9 +32,9 @@ describe('Authentication measures', () => {
         await expect(page.getByRole('heading', { name: /Sign up/})).toBeVisible()
         await page.getByTestId('username').fill('abram')
         await page.getByTestId('email').fill('acmarcelo2@up.edu.ph')
-        await page.getByTestId('password').fill('Hakdog2%')
+        await page.getByTestId('password').fill('Abc123!?')
         await page.getByRole('button', { name : 'Sign up'}).click()
-        await expect(page.getByRole('heading', { name: /Update/})).toBeVisible()
+        await expect(page.getByRole('heading', { name: /Update/ })).toBeVisible()
 
         await page.getByTestId('first-name').fill('ABRAM')
         await page.getByTestId('last-name').fill('MARCELO')
@@ -43,31 +42,59 @@ describe('Authentication measures', () => {
 
         await expect(page.getByRole('heading', { name: /HELLO/ })).toBeVisible()
 
-        await page.getByRole('button', { name: 'Logout' }).click()
+        await page.getByRole('button', { name: /Delete/ }).click()
         await expect(page.getByRole('heading', { name: /Toruts/ })).toBeVisible()
 
-    })
+    })  
 
     test('user fails to login with wrong password', async ({ page }) => {
-        await loginWith(page, 'abram', 'blow-kesha')
+        // await loginWith(page, 'abram', 'blow-kesha')
+        
+        await page.getByTestId('username').fill('abram');
+        await page.getByTestId('password').fill('blow-kesha');
+        await page.getByRole('button', { name: 'Login' }).click();
+
         await expect(page.getByRole('heading', { name: /Toruts/ })).toBeVisible()
     })
 
     test('user can log in', async ({ page }) => {
-        await loginWith(page, 'abram', 'Hakdog2%')
+        // await loginWith(page, 'abram', 'Abc123!?')
+        
+        await page.getByTestId('username').fill('admin');
+        await page.getByTestId('password').fill('Abc123!?');
+        await page.getByRole('button', { name: 'Login' }).click();
+
         await expect(page.getByRole('heading', { name: /HELLO/})).toBeVisible()
         await page.getByRole('button', { name: 'Logout' }).click()
         await expect(page.getByRole('heading', { name: /Toruts/ })).toBeVisible()
     })
 
     test('user can delete account', async ({ page }) => {
-        await loginWith(page, 'abram', 'Hakdog2%')
-        await page.getByRole('button', { name : 'Delete Profile'}).click()
+        // await loginWith(page, 'abram', 'Abc123!?')
+        
+        await page.getByTestId('username').fill('deleted-user');
+        await page.getByTestId('password').fill('Delete123!?');
+        await page.getByRole('button', { name: 'Login' }).click();
+
+        await expect(page.getByRole('heading', { name: /HELLO/})).toBeVisible()
+        await page.getByRole('button', { name : /Delete/}).click();
         await expect(page.getByRole('heading', { name: /Toruts/ })).toBeVisible()
+
+        await page.getByRole('button', { name : /Sign up/}).click()
+        await page.getByTestId('username').fill('deleted-user');
+        await page.getByTestId('email').fill('deleted@user.com');
+        await page.getByTestId('password').fill('Deleted123!?');
+
+        await page.getByRole('button', { name: /Sign up/ }).click();
     })
 
     test('user cannot login anymore after deleting account', async ({ page }) => {
-        await loginWith(page, 'abram', 'Hakdog2%')
+        // await loginWith(page, 'abram', 'Abc123!?')
+        
+        await page.getByTestId('username').fill('abram');
+        await page.getByTestId('password').fill('Abc123!?');
+        await page.getByRole('button', { name: 'Login' }).click();
+        
         await expect(page.getByRole('heading', { name: /Toruts/ })).toBeVisible()
     }) 
 })
