992321e545a33b85ffb80ccbdc471b360c105290
William Santos <w.santos.gg@gmail.com>
2025-02-19 13:42:16 +0800

    feat(dto): add create student request dto
    add CreateStudentRequestdto class to Dtos

---
+++ api/Dtos/Record/CreateStudentRequestDto.cs
@@ -0,0 +1,10 @@
+using api.Enums;
+
+namespace api.Dtos.Record
+{
+    public class CreateStudentRequestDto
+    {
+        public List<string>? AreasOfImprovement { get; set; }
+        public string? DegreeProgram { get; set; }
+    }
+}
