76c02d5dff6510d916237c04e1a53352e0986768
William Santos <w.santos.gg@gmail.com>
2025-02-19 12:57:40 +0800

    feat(records): add studentdto
    add StudentDto class to api.Dtos.Record

---
+++ api/Dtos/Record/StudentDto.cs
@@ -0,0 +1,16 @@
+using api.Enums;
+
+namespace api.Dtos.Record
+{
+    public class StudentDto
+    {
+        public int Id { get; set; }
+
+        public List<string>? AreasOfImprovement { get; set; }
+        public string? DegreeProgram { get; set; }
+
+        public required string UserId { get; set; }
+
+
+    }
+}
