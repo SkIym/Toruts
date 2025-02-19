56b5dcac1700a4d43f8285fb0f7bd3e657b421ac
William Santos <w.santos.gg@gmail.com>
2025-02-19 10:58:11 +0800

    feat(controllers): add student controller
    add a StudentController class to the controllers

---
+++ api/Controllers/StudentController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using api.Interfaces;
+using api.Models;
+using api.Data;
+using api.Mappers;
+
+
+namespace api.Controllers
+{
+    [Route("api/student")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly ApplicationDBContext _context;
+
+        // Construct our stuff
+        public StudentController(UserManager<User> userManager, ApplicationDBContext context)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var studentList = await _context.Student.ToListAsync();
+            // var student = studentList.Select(t=>t.S)
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("api/{username}/student")]
+        public async Task<IActionResult> GetByUsername([FromRoute] string username)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user == null)
+            {
+                return NotFound(username);
+            }
+
+            var student = await _context.Student.FirstOrDefaultAsync(t => t.UserId == user.Id);
+
+            if (tutor == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(student.ToStudentDto());
+
+        }
+    }
+
+}
