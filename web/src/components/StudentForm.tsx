02ba16111f59a8ebdb8e95a5bce61f2a819ed400
William Santos <w.santos.gg@gmail.com>
2025-02-22 13:47:50 +0800

    fix(components): transfer StudentInfoForm to StudentForm
    mind hazy today

--- web/src/components/StudentForm.tsx
+++ web/src/components/StudentForm.tsx
@@ -1,9 +1,47 @@
+import { useDispatch } from "react-redux"
+import { AppDispatch } from "../../store"
+import { signAsStudent } from "../reducers/userReducer"
+
 const StudentForm = () => {
 
-    return <>
-        Input fields lang here for student (not form!)
-        <button type="submit"> Upload</button>
-    </>
+    const { reset: areasReset, ...areas } = useField("text")
+    const { reset: degreeReset, ...degree } = useField("text")
+
+    const dispatch = useDispatch<AppDispatch>()
+
+    const handleInfo = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        try {
+            const loggedInUserJson = window.localStorage.getItem("loggedInUser")
+            if (loggedInUserJson == null) {
+                throw "not logged in"
+            }
+            await dispatch(signAsStudent("", {
+                areasOfImprovement: areas.value.split(","),
+                degreeProgram: degree.value
+            }))
+            areasReset()
+            degreeReset()
+        } catch (e) {
+            console.log(e)
+            return
+        }
+    }
+
+    return <div>
+        <span>Student Info Form</span>
+        <form onSubmit={handleInfo} id="student-info">
+            <div>
+                <span>Areas of Improvement:</span>
+                <input {...areas} data-testid="areas" />
+            </div>
+            <div>
+                <span>Degree Program</span>
+                <input {...degree} data-testid="areas" />
+            </div>
+            <button type="submit"> Upload</button>
+        </form>
+    </div>
 }
 
 export default StudentForm
