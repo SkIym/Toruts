e4fde48ee31c4e9fe17a0cea7a9e5de8c47d3aae
William Santos <w.santos.gg@gmail.com>
2025-02-22 12:45:59 +0800

    feat(components): add title to StudentInfoForm

--- web/src/components/StudentInfoForm.tsx
+++ web/src/components/StudentInfoForm.tsx
@@ -1,4 +1,8 @@
-import { useField } from "../hooks"
+import { useDispatch } from "react-redux"
+import { useErrorNotification, useField } from "../hooks"
+import { AppDispatch } from "../../store"
+import { useNavigate } from "react-router-dom"
+import { signAsStudent } from "../reducers/userReducer"
 
 const StudentInfoForm = () => {
 
@@ -27,6 +31,7 @@ const StudentInfoForm = () => {
     }
 
     return <div>
+        <span>Student Info Form</span>
         <form onSubmit={handleInfo} id="student-info">
             <div>
                 <span>Areas of Improvement:</span>
