3b612c91bccafbcc94ffad776f400540e1013ce0
William Santos <w.santos.gg@gmail.com>
2025-02-23 21:18:59 +0800

    refactor(services): change return type of search

--- web/src/services/tutor.ts
+++ web/src/services/tutor.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { serverURL } from "../constants";
-import { TutorInfo, TutorInfoWithoutId } from "../types";
+import { TutorInfo, TutorInfoWithoutId, TutorResult } from "../types";
 
 const url = serverURL + "tutors"
 
@@ -13,7 +13,7 @@ const create = async (username: string, creds: TutorInfoWithoutId) => {
 
 const search = async (searchTerm: string) => {
     console.log("Searching tutors")
-    const { data } = await axios.get<TutorInfo[]>(`${url}/search/${searchTerm}`)
+    const { data } = await axios.get<TutorResult[]>(`${url}/search/${searchTerm}`)
     return data
 
 }
