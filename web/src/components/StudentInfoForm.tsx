f100b9c37a64e358b355bb8f7d9d747a955f3e92
William Santos <w.santos.gg@gmail.com>
2025-02-23 13:49:22 +0800

    refactor(components): move student-info-form to student-form

--- web/src/components/StudentInfoForm.tsx
+++ web/src/components/StudentInfoForm.tsx
@@ -6,44 +6,7 @@ import { signAsStudent } from "../reducers/userReducer"
 
 const StudentInfoForm = () => {
 
-    const { reset: areasReset, ...areas } = useField("text")
-    const { reset: degreeReset, ...degree } = useField("text")
 
-    const dispatch = useDispatch<AppDispatch>()
-
-    const handleInfo = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        try {
-            const loggedInUserJson = window.localStorage.getItem("loggedInUser")
-            if (loggedInUserJson == null) {
-                throw "not logged in"
-            }
-            await dispatch(signAsStudent("", {
-                areasOfImprovement: areas.value.split(","),
-                degreeProgram: degree.value
-            }))
-            areasReset()
-            degreeReset()
-        } catch (e) {
-            console.log(e)
-            return
-        }
-    }
-
-    return <div>
-        <span>Student Info Form</span>
-        <form onSubmit={handleInfo} id="student-info">
-            <div>
-                <span>Areas of Improvement:</span>
-                <input {...areas} data-testid="areas" />
-            </div>
-            <div>
-                <span>Degree Program</span>
-                <input {...degree} data-testid="areas" />
-            </div>
-            <button type="submit"> Upload</button>
-        </form>
-    </div>
 
 }
 
