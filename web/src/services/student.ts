6e93819c346c5201b7fda7bfcfb463f273bc302a
William Santos <w.santos.gg@gmail.com>
2025-02-23 13:44:37 +0800

    fix(services): fix create and update errors
    add creds to post and put function arguments

--- web/src/services/student.ts
+++ web/src/services/student.ts
@@ -6,13 +6,13 @@ const url = serverURL + "student"
 
 const create = async (username: string, creds: StudentInfoWithoutId) => {
     console.log("Requesting to create student account")
-    const { data } = await axios.post<StudentInfo>(`${url}/create/${username}`)
+    const { data } = await axios.post<StudentInfo>(`${url}/create/${username}`, creds)
     return data
 }
 
 const update = async (username: string, creds: StudentInfoWithoutId) => {
     console.log("Requesting to update student account")
-    const { data } = await axios.put<StudentInfo>(`${url}/update/${username}`)
+    const { data } = await axios.put<StudentInfo>(`${url}/update/${username}`, creds)
     return data
 }
 
