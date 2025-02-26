a14a915942dcbe1da1549ac57d76ef9088dd4d43
Skiym <abramjosh13@gmail.com>
2025-02-26 20:36:18 +0800

    refactor(UpdateTutorDto): make fields optional

--- api/Dtos/Record/TutorDto.cs
+++ api/Dtos/Record/TutorDto.cs
@@ -28,17 +28,14 @@ namespace api.Dtos.Record
 
     public class UpdateTutorDto
     {
-        public string EducAttainment { get; set; } 
-        [Required]
-        public LearningModal LearningMode { get; set; } 
-        [Required]
-        public string Venue { get; set; }
-        [Required]
-        public double Price { get; set; }
+        public string? EducAttainment { get; set; } 
+        public LearningModal? LearningMode { get; set; } 
+        public string? Venue { get; set; }
+        public double? Price { get; set; }
         public List<string>? AreasOfExpertise { get; set; }
         public string? TutoringExperiences { get; set; } 
         public string? Availability { get; set; }
         public string? PortraitUrl { get; set; } 
-        public Status Status { get; set; } 
+        public Status? Status { get; set; } 
     }
 }
