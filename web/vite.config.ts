2895a88045f45b324ad8916ee1f869afde844df4
Skiym <abramjosh13@gmail.com>
2025-03-08 14:56:46 +0800

    feat:add shadcn-ui + tailwind config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,19 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  preview: {
-    host: true,
-    port: 5173,
-    strictPort: true,
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-  server: {
-    host: true,
-    port: 5173,
-    strictPort: true,
-    origin: "http://0.0.0.0:5173"
-  }
 })
