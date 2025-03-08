398df629e4fe4e65f37d5ecd47952f6e480b1672
Skiym <abramjosh13@gmail.com>
2025-03-08 19:46:08 +0800

    style: background changes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -27,10 +27,11 @@ const App = () => {
     }
 
     return isReady ? (
-        <div>
+        <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
             <Outlet />
             <Toaster />
         </div>
+            
     ) : null
 }
 
--- web/src/components/pages/LoginPage.tsx
+++ web/src/components/pages/LoginPage.tsx
@@ -3,8 +3,8 @@ import LoginForm from "../templates/LoginForm"
 const LoginPage = () => {
 
     return <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 flex-col gap-y-9">
-            <h1 className="page-title">Welcome to Toruts, ka-peyups!</h1>
-            <div className="w-full max-w-sm shadow-lg rounded-sm p-8">
+            <h1 className="page-title font-bold text-gray-900">Welcome to Toruts, ka-peyups!</h1>
+            <div className="w-full max-w-sm shadow-lg rounded-sm p-8 bg-white">
                 <LoginForm />
             </div>
     </div>
