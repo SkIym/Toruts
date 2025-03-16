b70436df4ecd59ca20960cc27cd2b8b90037071f
Skiym <abramjosh13@gmail.com>
2025-03-16 18:34:24 +0800

    style(forms): clean up login and signup forms

--- web/src/components/pages/SignupPage.tsx
+++ web/src/components/pages/SignupPage.tsx
@@ -18,9 +18,8 @@ const SignupPage = ({ type }: { type: UserType | null }) => {
   }
 
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 flex-col gap-y-9">
-    <h1 className="page-title font-bold text-gray-900">Sign up</h1>
-      <div className="w-full max-w-4xl shadow-lg rounded-sm p-8 bg-white">
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 flex-col">
+      <div className="w-full max-w-4xl">
         {child}
       </div>
     </div>
--- web/src/components/templates/LoginForm.tsx
+++ web/src/components/templates/LoginForm.tsx
@@ -14,6 +14,13 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 
 
@@ -44,48 +51,57 @@ const LoginForm = () => {
 
     return (
         <div id="login">
-            <Form {...loginForm}>
-                <form onSubmit={loginForm.handleSubmit(handleLogin)} data-testid="form" id="login-form" className="space-y-8">
-                    <FormField
-                        control = {loginForm.control}
-                        name = "username"
-                        render ={({ field }) => (
-                            <FormItem>
-                                <div className="flex flex-row justify-between"> 
-                                    <FormLabel>Username</FormLabel>
-                                    <FormMessage />
-                                </div>
-                                <FormControl>
-                                    <Input placeholder="Username" {...field} data-test-id="username"/>
-                                </FormControl>
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control = {loginForm.control}
-                        name = "password"
-                        render ={({ field }) => (
-                            <FormItem>
-                                <div className="flex flex-row justify-between"> 
-                                    <FormLabel>Password</FormLabel>
-                                    <FormMessage />
-                                </div>
-                                <FormControl>
-                                    <Input placeholder="Password" {...field} data-test-id="password" type="password" />
-                                </FormControl>
-                            </FormItem>
-                        )}
-                    />
-                    <div className="flex flex-row gap-4 justify-between">
-                        <Button type="submit" data-testid="login-button">Login</Button>
-                        <Button type='button' variant="outline" onClick={() => navigate("/signup")} data-testid="signup-button">
-                            Sign up instead
-                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-                        </Button>
-                    </div>
-                </form>
-            </Form>
-            
+            <Card>
+                <CardHeader>
+                <CardTitle className="text-2xl">Login</CardTitle>
+                <CardDescription>
+                     Enter your username below to login to your account
+                </CardDescription>
+                </CardHeader>
+                <CardContent>
+                <Form {...loginForm}>
+                    <form onSubmit={loginForm.handleSubmit(handleLogin)} data-testid="form" id="login-form" className="space-y-8">
+                        <FormField
+                            control = {loginForm.control}
+                            name = "username"
+                            render ={({ field }) => (
+                                <FormItem>
+                                    <div className="flex flex-row justify-between"> 
+                                        <FormLabel>Username</FormLabel>
+                                        <FormMessage />
+                                    </div>
+                                    <FormControl>
+                                        <Input placeholder="Username" {...field} data-test-id="username"/>
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control = {loginForm.control}
+                            name = "password"
+                            render ={({ field }) => (
+                                <FormItem>
+                                    <div className="flex flex-row justify-between"> 
+                                        <FormLabel>Password</FormLabel>
+                                        <FormMessage />
+                                    </div>
+                                    <FormControl>
+                                        <Input placeholder="Password" {...field} data-test-id="password" type="password" />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <div className="flex flex-row gap-4 justify-between">
+                            <Button type="submit" data-testid="login-button">Login</Button>
+                            <Button type='button' variant="outline" onClick={() => navigate("/signup")} data-testid="signup-button">
+                                Sign up instead
+                            <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
+                </CardContent>
+            </Card>
         </div>
     );
 };
