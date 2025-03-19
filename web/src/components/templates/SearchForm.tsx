297c7207244e7f9e69edf43a9cdf321b5e126897
William Santos <w.santos.gg@gmail.com>
2025-03-19 18:46:26 +0800

    feat(components): add layout to home page

--- web/src/components/templates/SearchForm.tsx
+++ web/src/components/templates/SearchForm.tsx
@@ -2,12 +2,15 @@ import { useDispatch } from "react-redux";
 import { useField } from "../../hooks";
 import { AppDispatch } from "../../../store";
 import { getTutors } from "../../reducers/userReducer";
-import { TutorResult } from "../../types";
 import { useEffect, useState } from "react";
 import { Input } from "../ui/input";
+
 import TutorSearchResult from "./TutorSearchResult";
+import { TutorDetails } from "./TutorDetails";
 
 import tutorService from "../../services/tutor";
+
+import { Card, CardHeader, CardTitle, CardContent } from "../ui/card";
 
 const SearchForm = () => {
 	const { ...search } = useField("text");
@@ -19,7 +22,7 @@ const SearchForm = () => {
 	const [tutors, setTutors] = useState<TutorResult[]>([]);
 	const [isReady, setIsReady] = useState(false);
 
-	const [selectedTutor, setSelectedTutor] = useState(null);
+	const [selectedTutor, setSelectedTutor] = useState<TutorResult | null>(null);
 
 	useEffect(() => {
 		const getTutors = async () => {
@@ -61,33 +64,48 @@ const SearchForm = () => {
 		}
 	};
 
+	console.log(selectedTutor);
+
 	return (
-		<div className="flex h-200">
+		<div className="flex h-200 gap-4">
 			{/* Search Results */}
 			<div className="min-w-1/3">
-				<form onSubmit={handleSearch} className="flex flex-col gap-2">
-					<div className="flex pr-10 gap-4">
-						<Input
-							{...search}
-							data-testid="search"
-							placeholder="John Doe, Ellen Joe, etc."
-						/>
-						<button data-testid="search-button"> Search </button>
-					</div>
-					<div className="flex justify-between">
-						<span>Price: </span>{" "}
-						<Input {...minPrice} data-testid="minPrice" className="w-1/4" />
-						<span> - </span>
-						<Input {...maxPrice} data-testid="maxPrice" className="w-1/4" />
-					</div>
-					<br />
-				</form>
+				<Card>
+					<CardContent>
+						<form onSubmit={handleSearch} className="flex flex-col gap-2">
+							<div className="flex pr-10 gap-4">
+								<Input
+									{...search}
+									data-testid="search"
+									placeholder="John Doe, Ellen Joe, etc."
+								/>
+								<button data-testid="search-button"> Search </button>
+							</div>
+							<div className="flex justify-between">
+								<span>Price: </span>{" "}
+								<Input {...minPrice} data-testid="minPrice" className="w-1/4" />
+								<span> - </span>
+								<Input {...maxPrice} data-testid="maxPrice" className="w-1/4" />
+							</div>
+							<br />
+						</form>
+					</CardContent>
+				</Card>
+				<br />
 				{isReady ? (
-					<div className="flex flex-col flex-grow gap-2 max-h-160 overflow-y-auto">
+					<div className="flex flex-col flex-grow gap-2 max-h-150 overflow-y-auto">
 						{tutors.length === 0
 							? "No tutors found :(. The name or subject you're trying to find is non-existent. Try adjusting your filters instead. "
 							: tutors.map((tutor) => {
-									return <TutorSearchResult {...tutor} />;
+									return (
+										// biome-ignore lint/correctness/useJsxKeyInIterable: <explanation>
+										<TutorSearchResult
+											{...tutor}
+											callback={() => {
+												setSelectedTutor(tutor);
+											}}
+										/>
+									);
 								})}
 					</div>
 				) : (
@@ -96,8 +114,29 @@ const SearchForm = () => {
 			</div>
 
 			{/*Tutor Results*/}
-			<div className="min-w-2/3 p-6 bg-white border-black border-2 min-h-160">
-				<div className="">Hello I am a tutor</div>
+			<div className="min-w-2/3 p-6 bg-white border-2 min-h-160 rounded">
+				<div className="">
+					{selectedTutor ? (
+						<div>
+							<img
+								src="https://img.freepik.com/free-photo/serious-young-african-man-standing-isolated_171337-9633.jpg"
+								alt=""
+								className="h-20 w-20 rounded-full object-cover"
+							/>
+							<b>
+								{selectedTutor.user.firstName} {selectedTutor.user.lastName}
+							</b>
+
+							{selectedTutor.price}
+							{selectedTutor.availability}
+							{selectedTutor.areasOfExpertise}
+							{selectedTutor.educAttainment}
+							{selectedTutor.learningMode}
+						</div>
+					) : (
+						<div>Please select a tutor</div>
+					)}
+				</div>
 			</div>
 		</div>
 	);
