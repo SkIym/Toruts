1c07cbd972c2d9112fc7f6f4d1a49b19a61679c7
William Santos <w.santos.gg@gmail.com>
2025-03-22 17:51:43 +0800

    fix(homepage): add missing navbar after merge
    wrong merge :((

--- web/src/components/pages/HomePage.tsx
+++ web/src/components/pages/HomePage.tsx
@@ -1,15 +1,17 @@
-import { Link } from "react-router-dom"
-import SearchForm from "../templates/SearchForm"
-import { PATH, TEST } from "@/constants"
+import { Link } from "react-router-dom";
+import SearchForm from "../templates/SearchForm";
+import { PATH, TEST } from "@/constants";
+import Navbar from "../ui/navbar";
 
 const HomePage = () => {
-    return (
-        <div data-testid={TEST.page('home')}>
-            <h1>Toruts</h1>
-            <Link to={`${PATH.PROFILE.default}?`}>
-                <h3>Profile</h3>
-            </Link>
-            <SearchForm />
-        </div>
+	return (
+		<div data-testid={TEST.page("home")}>
+			<Navbar />
+			<div className="pl-50">
+				<SearchForm />
+			</div>
+		</div>
+	);
+};
 
 export default HomePage;
