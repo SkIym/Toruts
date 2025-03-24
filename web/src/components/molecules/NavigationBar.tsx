ef460af1e6e617f1981a5f0d3ca7a1cf094b24cf
Nixlyhilist <nicosebastianramos@gmail.com>
2025-03-25 04:48:13 +0800

    fix(navbar): font and flex

--- web/src/components/molecules/NavigationBar.tsx
+++ web/src/components/molecules/NavigationBar.tsx
@@ -17,27 +17,34 @@ const NavBar = () => {
         }
     };
 
+    const hideNavBarRoutes = ["/login", "/signup"];
+    const shouldHideNavBar = hideNavBarRoutes.includes(location.pathname);
+
     return (
-        <nav className="w-full bg-gray-800 p-5">
+        <nav className="w-full bg-gray-800 p-5 h-1/11.5">
             <div className="flex flex-row justify-between items-center mr-10 ml-10">
                 <div className="flex items-center">
-                    <h1 className="pacifico-regular text-white text-xl m-2">Toruts</h1>
+                    <h1 className="font-pacifico text-white text-3xl m-2">Toruts</h1>
                 </div>
-                <form className="flex flex-row basis-1/3 space-x-10 h-4/5 text-black" onSubmit={handleSearchSubmit}>
-                    <Input
-                        type="text"
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                        placeholder="Search..."
-                        className="text-black bg-white w-full"
-                        onKeyDown={handleKeyDown}
-                    />
-                    <button type="submit" className="text-white">Search</button>
-                </form>
-                <div className="flex flex-row space-x-25 items-center">
-                    <Link to="/" className="text-white">Home</Link>
-                    <Link to="/profile" className="text-white">Profile</Link>
-                </div>
+                {!shouldHideNavBar && (
+                    <form className="flex flex-row basis-1/3 space-x-10 h-4/5 text-black" onSubmit={handleSearchSubmit}>
+                        <Input
+                            type="text"
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                            placeholder="Search..."
+                            className="text-black bg-white w-full"
+                            onKeyDown={handleKeyDown}
+                        />
+                        <button type="submit" className="text-white">Search</button>
+                    </form>
+                )}
+                {!shouldHideNavBar && (
+                    <div className="flex flex-row space-x-25 items-center">
+                        <Link to="/" className="text-white">Home</Link>
+                        <Link to="/profile" className="text-white">Profile</Link>
+                    </div>
+                )}
             </div>
         </nav>
     );
