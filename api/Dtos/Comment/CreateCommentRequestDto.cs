111f6bf21a5c694e66246a22a34a4fd3aaf6a6fb
Skiym <abramjosh13@gmail.com>
2025-04-05 14:58:05 +0800

    feat(Dtos): add dto for commment create requests

---
+++ api/Dtos/Comment/CreateCommentRequestDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos.Comment
+{
+    public class CreateCommentRequestDto
+    {
+        public required int TutorId { get; set; }
+        public required string Text { get; set; }
+        public required int Helpfulness { get; set; }
+        public required int Pedagogy { get; set; }
+        public required int Easiness { get; set; }
+    }
+}
