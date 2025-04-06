e6fb58c4ab888c1762474e5a4f50bf17213804c8
Skiym <abramjosh13@gmail.com>
2025-04-06 15:05:17 +0800

    feat(Comments): add transfer objects and mappers for Comment entities

---
+++ api/Dtos/Comment/CommentDto.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos.Comment
+{
+    public class CommentDto
+    {
+        public required string CommenterFirstName { get; set; }
+        public required string CommenterLastName { get; set; }
+        public required int CommenterId { get; set; }
+        public required string Text { get; set; }
+        public required int Helpfulness { get; set; }
+        public required int Pedagogy { get; set; }
+        public required int Easiness { get; set; }
+
+    }
+}
+
+// commenterFirstName: string,
+    // commenterLastName: string,
+    // commenterId: number,
+    // text: string,
+    // helpfulness: number,
+    // pedagogy: number,
+    // easiness: number
