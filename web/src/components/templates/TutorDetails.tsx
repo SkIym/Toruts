4344704dd60dd80c08bc9f2a7d470267b3566757
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-04-08 22:58:41 +0800

    fix: fix comments not refreshing after selecting another tutor

--- web/src/components/templates/TutorDetails.tsx
+++ web/src/components/templates/TutorDetails.tsx
@@ -44,7 +44,7 @@ const TutorDetails = ({ selectedTutor }: { selectedTutor: TutorResult }) => {
 
 	useEffect(() => {
 		getComments()
-	}, [])
+	}, [selectedTutor])
 	console.log(comments)
 
 	const user = useSelector((state: RootState) => state.user);
