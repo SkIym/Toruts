b8c60a82f859292486ff47323733e2506132a07b
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-04-08 16:11:08 +0800

    feat: add shadcn form to comment form

--- web/src/components/templates/CommentForm.tsx
+++ web/src/components/templates/CommentForm.tsx
@@ -2,22 +2,101 @@ import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "store";
+import { useField } from "@/hooks";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 
 const CommentForm = () => {
-    return (
-        <form className="items-end mb-10">
-            <div className="flex mb-5 gap-2">
+    const dispatch = useDispatch<AppDispatch>()
 
-                <Label>Pedagogy</Label>
-                <Input />
-                <Label>Helpfulness</Label>
-                <Input />
-                <Label>Easiness</Label>
-                <Input />
-            </div>
-            <Textarea placeholder="Type your comment here" className="mb-5" />
-            <Button> Post Comment</Button>
-        </form>
+    const CommentSchema = z.object({
+        pedagogy: z.number(),
+        helpfulness: z.number(),
+        easiness: z.number(),
+        comment: z.string().nonempty({ message: "required" })
+    })
+
+    type CommentSchemaType = z.infer<typeof CommentSchema>
+
+    const commentForm = useForm<CommentSchemaType>({
+        resolver: zodResolver(CommentSchema)
+    })
+
+    const handleCommentSubmit: SubmitHandler<CommentSchemaType> = async (formData) => {
+        console.log(formData)
+    }
+
+    return (
+        <Form {...commentForm} >
+            <form
+                className="items-end mb-10"
+                onSubmit={commentForm.handleSubmit(handleCommentSubmit)}
+                id="comment-form"
+            >
+                <div className="flex mb-5 mt-5 gap-2">
+                    <FormField
+                        control={commentForm.control}
+                        name="pedagogy"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Pedagogy</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        {...field}
+                                    />
+                                </FormControl>
+
+                            </FormItem>
+                        )} />
+
+                    <FormField
+                        control={commentForm.control}
+                        name="helpfulness"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Helpfulness</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        {...field}
+                                    />
+                                </FormControl>
+
+                            </FormItem>
+                        )} />
+
+                    <FormField
+                        control={commentForm.control}
+                        name="easiness"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Easiness</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        {...field}
+                                    />
+                                </FormControl>
+
+                            </FormItem>
+                        )} />
+                </div>
+
+                <FormField control={commentForm.control} name="comment" render={({ field }) => (
+                    <FormItem>
+                        <FormControl>
+                            <Textarea placeholder="Type your comment here" className="mb-5" {...field} />
+                        </FormControl>
+
+                    </FormItem>
+
+                )} />
+
+                <Button type="submit">Post Comment</Button>
+            </form>
+        </Form>
     )
 }
 
