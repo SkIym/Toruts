76a61bd9ab5cff5f513a5ba5981a0373741d8322
Skiym <abramjosh13@gmail.com>
2025-08-22 13:40:24 +0800

    chore: temp test disable

--- tests/integration/obj/Debug/net9.0/integration.AssemblyInfo.cs
+++ tests/integration/obj/Debug/net9.0/integration.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("integration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ae62e9323386714af3f3f53bd8de8d36a4ffb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c589a1946282b0232daf510e665912e7e99a5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("integration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("integration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
